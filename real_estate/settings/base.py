9c9668926b4a623a948b89f07bc12ead7aabf595
mars-96 <mars73348@gmail.com>
2023-05-30 22:39:32 +0500

    feat: add custom user model

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -137,6 +137,8 @@ MEDIAFILES_DIR = []
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
 import logging
 import logging.config
 
